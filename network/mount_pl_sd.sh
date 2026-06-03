ee6c535d2e7716e9f49f6f3a1e840e96793a83df
jiacong sun <jiacong.sun@esat.kuleuven.be>
2026-06-03 18:16:39 +0200

    add network scripts

---
+++ network/mount_pl_sd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Copyright 2025 KU Leuven.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Author: Jiacong Sun <jiacong.sun@kuleuven.be>
+
+# Description: Mount the SD card partition to /mnt/pl_sd if not already mounted.
+
+mkdir -p /mnt/pl_sd
+if ! findmnt -rn -o TARGET | grep -q "^/mnt/pl_sd$"; then
+    mount /dev/mmcblk0p1 /mnt/pl_sd
+fi
