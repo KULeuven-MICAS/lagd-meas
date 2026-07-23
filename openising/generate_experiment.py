5c7d7f01eef3aacd2ef7b6340eb055f6dd081f61
sofiedeweer <sofie.deweer@esat.kuleuven.be>
2026-07-23 17:53:24 +0200

    fix: added new arguments

--- openising/generate_experiment.py
+++ openising/generate_experiment.py
@@ -6,13 +6,23 @@ from __init__ import toppath, current_dir
 from ising.api import get_hamiltonian_energy
 from save_model import store_run
 from mppi_experiment import mppi_experiment
-from send_chip import compile_data
+from send_chip import compile_data, send_chip
+from ising.stages.simulation_stage import Ans
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "-config_file", help="directory to yaml file for experiment.", type=str, default="MIMO_experiment/model_0"
+    "-config-file", help="directory to yaml file for experiment.", type=str, default="MIMO_experiment/model_0"
 )
-parser.add_argument("--logging_level", help="level of logging. Defaults to INFO", default=logging.INFO)
+parser.add_argument("--logging-level", help="level of logging. Defaults to INFO", default=logging.INFO)
+parser.add_argument(
+    "-simulate",
+    help="whether to simulate the data and compile or not simulate and send to chip",
+    default=True,
+    type=bool,
+)
+parser.add_argument("--nb-cores", help="The amount of cores to use on chip", type=int, default=1)
+parser.add_argument("--interface", help="The interface to send the data with", type=str, default="uart")
+parser.add_argument("--send-to-chip", default=False, type=bool)
 
 args = parser.parse_args()
 
@@ -29,7 +39,7 @@ experiment_config.update(base_config)
 experiment_config["benchmark"] = str(current_dir / args.config_file / "benchmark.yaml")
 save_folder = current_dir / args.config_file
 # ensure the amount of runs is even
-if experiment_config["nb_runs"] % 2!=0:
+if experiment_config["nb_runs"] % 2 != 0:
     experiment_config["nb_runs"] *= 2
 config_path = "./ising/inputs/config/config_experiment.yaml"
 openising_config = toppath / config_path
@@ -39,11 +49,18 @@ with openising_config.open("w") as f:
 problem_type = experiment_config["problem_type"]
 # Start openising run
 if problem_type != "MPPI":
-    ans, _ = get_hamiltonian_energy(problem_type, config_path, args.logging_level)
-    # Store everything
-    ans.save(save_folder / "ans.pkl")
-    data_folders = store_run(ans, save_folder, problem_type)
-    # compile everything
-    compile_data(data_folders, 2)
+    if args.simulate:
+        if not (save_folder/"ans.pkl").exists():
+            ans, _ = get_hamiltonian_energy(problem_type, config_path, args.logging_level)
+            # Store everything
+            ans.save(save_folder / "ans.pkl")
+        else:
+            ans = Ans()
+            ans.load(save_folder/"ans.pkl")
+        data_folders = store_run(ans, save_folder, problem_type)
+        # compile everything
+        compile_data(data_folders, args.nb_cores)
+    else:
+        send_chip(save_folder, args.nb_cores, args.interface, args.send_to_chip)
 else:
     mppi_experiment(config_path, save_folder)
